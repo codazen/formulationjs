58da6b824fb6d74a58508c7cce193dc339b241ad
wkiyohara <wesley.kiyohara@codazen.com>
2015-11-06 15:42:34 -0800

    created src folder; added react libraries; added webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeModulesPath = path.join(__dirname, 'node_modules');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    root: [nodeModulesPath],
+    extensions: ['', '.js', '.jsx']
+  },
+  resolveLoader: {
+    root: nodeModulesPath
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$|\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
