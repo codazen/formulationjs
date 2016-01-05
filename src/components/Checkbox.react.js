d2838e5a241db8f8fc16bf181c90f47162407121
Steven Nguyen <steven.nguyen194@gmail.com>
2016-01-05 11:28:55 -0800

    added group checkbox description. clickable labels for checkbox.

--- src/components/Checkbox.react.js
+++ src/components/Checkbox.react.js
@@ -12,12 +12,14 @@ class Checkbox extends React.Component {
     var { checkbox, onChange, onClick, name } = this.props;
     return (
       <div>
-        <label htmlFor={checkbox.name} onClick={onClick}>{checkbox.label}</label>
+        {checkbox.groupLabel}
           {
             checkbox.options.map(function (option, index) {
               return  (
                 <div>
-                  <input type="checkbox" key={index} name={name} value={option.value} onClick={onClick} />{option.name}
+                  <label>
+                    <input type="checkbox" key={index} name={name} value={option.value} onClick={onClick} />{option.name}
+                  </label>
                 </div>
               );
             })
