d1b2266757126bfcab80b2fa05f715721e5ae05f
Steven Nguyen <steven.nguyen194@gmail.com>
2016-01-07 12:23:52 -0800

    fixed required and allow boolean statements. removed label from form def for checkbox

--- src/components/Textbox.react.js
+++ src/components/Textbox.react.js
@@ -8,11 +8,13 @@ class Textbox extends React.Component {
     super(props);
   }
 
+  
   render() {
     var { textbox, onChange } = this.props;
+    var star = textbox.required ? '*' : '';
     return (
       <div>
-        <label htmlFor={textbox.name}>{textbox.label}</label>
+        <label htmlFor={textbox.name}>{textbox.label}{star}</label>
         <input type="text" id={textbox.id} name={textbox.name} required={textbox.required} maxLength={textbox.maxlength} value={textbox.value} onChange={onChange} />
       </div>
     );
