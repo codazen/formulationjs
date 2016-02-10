1133b4a78b64bd1a1c4734918f6cd9eebd7d02da
Steven Nguyen <steven.nguyen194@gmail.com>
2016-02-10 13:08:43 -0800

    in progress...

--- src/components/DatePicker.react.js
+++ src/components/DatePicker.react.js
@@ -11,14 +11,27 @@ class DatePicker extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      //datepicker: this.props.DatePicker
+      startDate: null
+    };
+    this._handleChange = this._handleChange.bind(this);
   }
+
+  _handleChange(date) {}
 
   render() {
     var { datepicker, onChange, initialRender } = this.props;
-    var startDate = moment();
     return (
       <div>
-        <ReactDatePicker selected={startDate} onChange={onChange} />
+        {datepicker.label}
+        <ReactDatePicker 
+          selected={this.state.startDate} 
+          onChange={onChange}
+          value={datepicker.value}
+          dateFormat={datepicker.dateFormat}
+          maxLength={datepicker.maxlength}
+          placeholderText={datepicker.placeholderText}/>
       </div>
     );
   }
