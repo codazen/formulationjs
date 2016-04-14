41a9d6f0dfca3a5efa392433aa8ab10f61cd750b
sinwailam193 <sinwailam193@hotmail.com>
2016-04-14 14:57:44 -0700

    edited the style of the form

--- src/components/Form.react.js
+++ src/components/Form.react.js
@@ -152,13 +152,14 @@ class Form extends React.Component {
     var disabled = form.submitDisabled;
     var classes = classNames({
       'disabled' : disabled,
+      'form-style' : true
     });
     return (
       <section>
-        <h1 style={{ wordWrap: 'break-word' }}>{form.name}</h1>
-        <p style={{ wordWrap: 'break-word' }}>{form.body}</p>
+        <h1 className="form-style">{form.name}</h1>
+        <p className="form-style">{form.body}</p>
         <form onSubmit={this._handleSubmit}>
-          <div>
+          <div className="form-group">
             {
               form.elements ?
               form.elements.map((element, index) => {
