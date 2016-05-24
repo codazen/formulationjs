6be5bba8125da288c0b68666e1fbd99d630847b0
Michael Merchant <mmerchant@fb.com>
2016-05-23 18:02:11 -0700

    Add support for hosting site in a folder. Fix JS error when loaded on page without matching elements.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
-  var stickyNavTop = $('.scroll').offset().top;
-   
-  var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
-          
-    if (scrollTop > stickyNavTop) { 
-        $('.scroll').addClass('on-scroll');
-    } else {
-        $('.scroll').removeClass('on-scroll'); 
-    }
-  };
-   
-  stickyNav();
-   
-  $(window).scroll(function() {
+
+  var stickyElement = $('.scroll');
+
+  if (stickyElement && stickyElement.length > 0) {
+    var stickyNavTop = stickyElement.offset().top;
+
+    var stickyNav = function () {
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop > stickyNavTop) {
+        stickyElement.addClass('on-scroll');
+      } else {
+        stickyElement.removeClass('on-scroll');
+      }
+    };
+
     stickyNav();
-  });
+
+    $(window).scroll(function () {
+      stickyNav();
+    });
+  }
 });
