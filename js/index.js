9459914764b4213ec0b6f6a04192b51df5b387f4
sinwailam193 <sinwailam193@hotmail.com>
2016-06-01 10:23:05 -0700

    finish adding the formulation into the website

--- js/index.js
+++ js/index.js
@@ -1,17 +1,111 @@
 let textbox = {
-  "type": "textbox",
-  "data": {
-    "value": "",
-    "label": "Label",
-    "required": true,
-    "maxlength": "10",
-    "email": true,
-    "textboxState": true
-  }
+  "elements": [
+    {
+      "type": "textbox",
+      "data": {
+        "value": "",
+        "label": "Label",
+        "required": true,
+        "maxlength": "10",
+        "email": true,
+        "textboxState": true
+      }
+    }
+  ]
 };
-console.log(textbox);
-Formulation(textbox, "textbox");
+let obj1 = new Render(textbox);
+obj1.render("example1");
 
+let textarea = {
+  "elements": [
+    {
+      "type": "textarea",
+      "data": {
+        "value": "",
+        "label": "Label",
+        "required": true,
+        "maxlength": "300",
+        "textareaState": true
+      }
+    }
+  ]
+}
+
+let obj2 = new Render(textarea);
+obj2.render("example2");
+
+let checkbox = {
+  "elements": [
+    {
+      "type": "checkbox",
+      "data": {
+        "value": ["1"],
+        "groupLabel": "Checkbox group label",
+        "required": true,
+        "options": [
+          {
+            "name": "Option 1",
+            "value": 1
+          },
+          {
+            "name": "Option 2",
+            "value": 2
+          }
+        ]
+      }
+    }
+  ]
+}
+
+let obj3 = new Render(checkbox);
+obj3.render("example3");
+
+let dropdown = {
+  "elements": [
+    {
+      "type": "dropdown",
+      "placeholder": "Select...",
+      "data": {
+        "label": "Dropdown Label",
+        "options": [
+          {
+            "name": "Option 1",
+            "value": 1
+          },
+          {
+            "name": "Option 2",
+            "value": 2
+          }
+        ],
+        "value": "",
+        "required": true
+      }
+    }
+  ]
+}
+
+let obj4 = new Render(dropdown);
+obj4.render("example4");
+
+let datepicker = {
+  "elements": [
+    {
+      "type": "datepicker",
+      "data": {
+        "label": "Please select a due date",
+        "value": null,
+        "dateFormat": "DD/MM/YYYY",
+        "placeholderText": "dd/mm/yyyy",
+        "maxlength": "10",
+        "required": "false",
+        "range": "false"
+      }
+    }
+  ]
+}
+
+let obj5 = new Render(datepicker);
+obj5.render("example5");
 
 $(document).ready(function() {
   var stickyElement = $('.scroll');
